14168ce1f3d8485a85dd783160ad8d30b89e3251
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-24 14:41:42 +0200

    Toggle menu when user menu clicked

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
-import Avatar from "./Avatar";
+import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((prev) => !prev);
+  }, []);
+
   return (
     <div className="relative">
       <div className="flex items-center gap-3">
@@ -23,7 +30,7 @@ const UserMenu = () => {
             border-neutral-200 flex items-center
             gap-3 rounded-full cursor-pointer hover:shadow-md
             transition"
-          onClick={() => {}}
+          onClick={toggleOpen}
         >
           <AiOutlineMenu />
           <div className="hidden md:block">
@@ -31,6 +38,20 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div
+          className="
+          absolute rounded-xl shadow-md w-[40vw] md:w-3/4 
+          bg-white overflow-hidden right-0 top-12 text-sm"
+        >
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={() => {}} label="Sign up" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

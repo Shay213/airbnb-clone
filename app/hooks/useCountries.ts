3693f2700e98da326fb29a1b03158597c8c82bb9
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-25 21:48:20 +0200

    Create second step in form to add listing

---
+++ app/hooks/useCountries.ts
@@ -0,0 +1,23 @@
+import countries from "world-countries";
+
+const formattedCountries = countries.map((c) => ({
+  value: c.cca2,
+  label: c.name.common,
+  flag: c.flag,
+  latlng: c.latlng,
+  region: c.region,
+}));
+
+const useCountries = () => {
+  const getAll = () => formattedCountries;
+
+  const getByValue = (value: string) => {
+    return formattedCountries.find((c) => c.value === value);
+  };
+  return {
+    getAll,
+    getByValue,
+  };
+};
+
+export default useCountries;
